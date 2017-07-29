bb3b7bde203784fea3d9a3d904d07e4e50b3019f
Claudio Rojas Rodriguez <claudio.dcv@gmail.com>
2017-07-29 17:06:40 -0400

    add utils.py

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django_extensions.db.fields import AutoSlugField
-from django.core.validators import RegexValidator
+from .utils import phone_regex
 
 
 class Person(models.Model):
     name = models.CharField(max_length=100)
     natural_key = AutoSlugField(populate_from='name')
-    user = models.OneToOneField(User, on_delete=models.SET_NULL, related_name="person", null=True, blank=True)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=20) # validators should be a list
-
+    user = models.OneToOneField(
+        User,
+        on_delete=models.SET_NULL,
+        related_name='person',
+        null=True, blank=True,
+    )
+    phone_number = models.CharField(
+        validators=[phone_regex],
+        blank=True, max_length=20,
+    )
 
 class Color(models.Model):
     name = models.CharField(max_length=100)
